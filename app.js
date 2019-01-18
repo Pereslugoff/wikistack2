2f58dac9f6566500fb9ed5ea02f9dce4da41efb9
David H. Friedman <david.h.friedman.nyc@gmail.com>
2019-01-18 12:37:27 -0500

    through sequelize

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const morgan = require('morgan');
 const express = require('express');
 const port = 3000;
 const mainPage = require('./views/main')
+const { db, Page, User } = require('./models')
 
 const app = express()
 
@@ -13,5 +14,15 @@ app.get('/', function(req, res, next){
   res.send(mainPage())
 })
 
+async function Start() {
+  try {
+    const page = await Page.sync();
+    const user = await User.sync();
 
-app.listen(port, () => console.log(`Wikistack listening on port ${port}!`))
+    app.listen(port, () => console.log(`Wikistack listening on port ${port}!`))
+  } catch (error) {
+    console.log(`DATABASE SYNC ERROR: ${error}`);
+  }
+}
+
+Start();
